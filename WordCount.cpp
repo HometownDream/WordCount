c796fc4517444202c90e0ca7c934dcc9c3d55299
HometownDream <64819164+HometownDream@users.noreply.github.com>
2021-05-04 18:50:39 +0800

    add comment

--- WordCount.cpp
+++ WordCount.cpp
@@ -2,34 +2,34 @@
 #include<string.h>
 #include <stdlib.h>
 
-void count_char(char *str) {
+void count_char(char *str) {//计算字符数
     int sum = 0;
     int ch;
     FILE* fp;
     
-    if ((fp = fopen(str, "r")) == NULL) {
+    if ((fp = fopen(str, "r")) == NULL) {//打开文件，如果文件不存在，则输出“Can't open xxx.txt”
         printf("Can't open %s\n", str);
         exit(1);
     }
-    while ((ch = fgetc(fp)) != EOF)
+    while ((ch = fgetc(fp)) != EOF)//把文本文件中的每个字符取出来，然后数量加一进行统计，直到文件结尾
     {
         sum++;
     }
     printf("字符数：%d",sum);
 }
 
-void count_word(char* str) {
+void count_word(char* str) {//计算单词数
     int sum = 0;
     int word = 0;
     
     int ch;
     FILE* fp;
 
-    if ((fp = fopen(str, "r")) == NULL) {
+    if ((fp = fopen(str, "r")) == NULL) {//打开文件，如果文件不存在，则输出“Can't open xxx.txt”
         printf("Can't open %s\n", str);
         exit(1);
     }
-    while ((ch = fgetc(fp)) != EOF)
+    while ((ch = fgetc(fp)) != EOF)//统计被‘,’或‘ ’分割的单词数
     {
         if (ch == ' '||ch==',')
             word = 0;
@@ -43,7 +43,7 @@ void count_word(char* str) {
 }
 
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[])//主函数
 {
         char* s1= argv[1];
         char* s2 = argv[2];
